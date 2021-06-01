dbef385b84ebea0363527f4321bf3a4e1c308f7e
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2021-06-01 16:30:48 +0300

    add 404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Button from '../components/_shared/button/button.svelte'
+  import pageNotFound from '../assets/images/page-not-found.svg'
+</script>
+
+<div class="page-not-found">
+  <div class="four">
+    <img src={pageNotFound} alt="page not found" />
+  </div>
+
+  <div class="btn-container">
+    <Button>Go Home</Button>
+  </div>
+</div>
+
+<style lang="scss">
+  .page-not-found {
+    padding: 32px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    height: 40vh;
+
+    .four {
+      width: 100%;
+    }
+
+    .btn-container {
+      width: 300px;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+    }
+    @media only screen and (min-width: 640px) {
+      height: 80vh;
+      .four {
+        width: 80%;
+      }
+    }
+
+    /* large */
+    @media only screen and (min-width: 1008px) {
+      height: 100vh;
+    }
+  }
+</style>
