5eea0fd993db35cac909ad8a16b93dafc0c0668f
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2021-09-06 09:11:37 +0300

    Revert "Upgrade dependencies & fix husky setup"
    This reverts commit 17592b7e81dd4e60fbef46d56b6e815ef840447c.

--- src/lib/seo-store.ts
+++ src/lib/seo-store.ts
@@ -1,9 +1,12 @@
 import { writable } from 'svelte/store'
 
 export const seo = writable<{
-  title: string
-  description?: string
+  title: string,
+  description?: string,
 }>({
-  title: 'Kelal Tech PLC',
-  description: `Kelal Tech PLC is an international, remote IT agency based in Ethiopia working on a wide range of software products—from websites and mobile apps to fully integrated custom systems and open source libraries.`, // default description
+  title: 'Kelal Tech PLC', // default title
+  description: `Kelal Tech PLC is an international, remote IT agency based in 
+  Ethiopia working on a wide range of software products—from websites and mobile 
+  apps to fully integrated custom systems and 
+  open source libraries.`, // default description
 })
